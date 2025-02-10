4105013cf7ad5efcdc113ce0f50cd5fa750bc896
douglasbagambe <douglasbagambe4@gmail.com>
2025-02-10 17:16:30 +0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import React from "react";
+import { Web3ProviderNetwork } from "@web3-react/network";
+import { Web3Provider } from "@ethersproject/providers";
+import Home from "./components/pages/Home";
 import { Web3ReactProvider } from "@web3-react/core";
-import { ethers } from "ethers";
-import Home from "@/components/pages/Home";
 
 function getLibrary(provider: any) {
-  const library = new ethers.providers.Web3Provider(provider);
+  const library = new Web3Provider(provider);
   library.pollingInterval = 12000;
   return library;
 }
 
-export default function Page() {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
+const App = () => (
+  <Web3ProviderNetwork getLibrary={getLibrary}>
+    <Web3ReactProvider connectors={[]}>
       <Home />
     </Web3ReactProvider>
-  );
-}
+  </Web3ProviderNetwork>
+);
+
+export default App;
